ac8739a3291ad4c0560d0fe33caeafdfd8169ae2
qichengzx <qichengzx@gmail.com>
2019-05-09 18:52:07 +0800

    contains duplicate

---
+++ algorithms/contains-duplicate/main.go
@@ -0,0 +1,13 @@
+package main
+
+func containsDuplicate(nums []int) bool {
+	m := make(map[int]struct{}, len(nums))
+	for _, v := range nums {
+		if _, ok := m[v]; ok {
+			return true
+		} else {
+			m[v] = struct{}{}
+		}
+	}
+	return false
+}
